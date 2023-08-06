cfe2523e0821fb1c891073686bcc4911cfb1c2de
Chiba Yuki <ychibavolleyball@gmail.com>
2023-08-07 00:40:44 +0900

    push 0807

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #管理者
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+  }
+  devise_scope :admin do
+    get '/admins/sign_out' => 'devise/sessions#destroy'
+  end
+
+  #ユーザー
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
+  root to: "homes#top"
+
 end
